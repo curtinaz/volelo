c0793882d81cf8bc4bf614f5b8cae6df1aadeb82
Cristian Curtinaz <curtinazif@gmail.com>
2024-02-25 19:01:24 -0300

    v1.0.0

--- frontend/ajax/users.js
+++ frontend/ajax/users.js
@@ -1,11 +1,27 @@
-import { get } from './ajax_base.js';
+import { get, post } from './ajax_base.js';
 
 function searchPlayer(username, successFunction, errorFunction) {
   get(`/api/users?search=${username}`, {},
-  successFunction,
-  errorFunction,
-  false,
-);
+    successFunction,
+    errorFunction,
+    false,
+  );
 }
 
-export { searchPlayer };
+function balanceTeam(players, successFunction, errorFunction) {
+  post(`/api/users/balancer`, players,
+    successFunction,
+    errorFunction,
+    false,
+  );
+}
+
+function updateElo(match, successFunction, errorFunction) {
+  post(`/api/users/eloUpdate`, match,
+    successFunction,
+    errorFunction,
+    false,
+  );
+}
+
+export { searchPlayer, balanceTeam, updateElo };
